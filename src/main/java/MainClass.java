8411f42c4fac2c66676ab33e1a6b049709ae9129
AlexZhuravlev <alex.zhuravlev89@gmail.com>
2020-03-04 14:50:16 +0200

    lesson 41

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainClass {
     public static void main(String[] args) {
@@ -9,8 +13,34 @@ public class MainClass {
 
 //        WebDriver driver = new FirefoxDriver();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // implicitlyWait. Дефолтный таймаут, максимальное время для драйвера на поиск элемента.
+                                                                            // Если найден раньше - действие выполнится сразу.
+                                                                            // Если не найден - ошибка. Указывется один раз в начале и по умолчанию применяется ко всем поискам элементов
 
-        driver.get("https://mvnrepository.com/");
+
+
+        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(800,600));
+        driver.manage().window().setPosition(new Point(2000,0));
+
+        driver.get("https://google.com/");
+        driver.navigate().to("https://selenium.dev"); //альтернативный метод навигации. Метод get пердпочтительней
+
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+
+
+
+
+        driver.quit();
+
+
+
 
 
 
